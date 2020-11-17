8cbbb9c09916fb63ec1a40ef42bc656a6400fd6e
= <navneil2006@gmail.com>
2020-11-17 21:13:29 +1100

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # NavTheNugget
-Hey There! I am Nav or NavTheNugget, I am a teenage programmer, I code after school and sometimes before school as well. I have been coding since the age of since and my first programming language was C. Then I learned C++ and Then Java and then Python. I recently learned Javascript and Typescript and now develops Web Apps using React, Typescript and Express.
+Hey There! I am Nav or NavTheNugget, I am a teenage programmer, I code after school and sometimes before school as well. I have been coding since the age of eight, my first programming language was C. Then I learned C++ and Then Java and then Python. I recently learned Javascript and Typescript and now develop Web Apps using React, Typescript and Express.
 
 ## Game Development
 I have basic knowledge of how computer graphics work, I have made several render engines, which are now long gone in old external SSDs. I used C++, GLFW and GLEW for my render engines.
@@ -11,21 +11,4 @@ I have also abstracted networking layers of Winsock and Unix Sockets and integra
 I have recently started developing Web Apps, Making complex architectures which were out of my comfort zone, programming APIs, React Apps, etc. This is the time where I actually started becoming a pain in the ass for my teacher(some people know who the teacher is), by making websites about him and completely making a joke out of the assignments.
 
 ## Conclusion
-This is basically what I have learned in programming so far, this repo actually contains my own personal website where I make a complete joke out of myself to make people laugh... I have actually added a few social media links at the end of the web page, so be sure to check them out, to see how big of a dumbass I am myself.
-
-### Project ideas
-Down Below is the List of Project Ideas that I came up with in 2 minutes because I am lazy, I would keep adding ideas to this list:
-##### Multiplayer Game
-Softwares to Build
-- UDP Server
-Programming Language of Choice: C++
-- Game
-Tools that can be used: Unity, Unreal Engine, Godot, OpenGL, SDL2, LWJGL, literally any game engine or framework.
-
-**Deploy**
-Deploy the Server on a VPS Server, choose from list down below:
-- High Frequency Compute($6) from [Vultr](https://www.vultr.com/products/high-frequency-compute/)
-- [Digitalocean](https://www.digitalocean.com/pricing/)
-
-
-Any VPS can be used, these are the ones I recommend for this project.
+This is basically what I have learned in programming so far, this repo actually contains my own personal website where I make a complete joke out of myself to make people laugh... I have actually added a few social media links at the end of the web page, so be sure to check them out, to see how big of a dumbass I am.
--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import './app.scss';
 import Modal from './components/modal';
 
 const App = () => {
-    const [show, setShow] = useState<Boolean>(false);
+    const [show, setShow] = useState<boolean>(false);
+    const [affiliate, setAffiliate] = useState<boolean>(false);
+
     const closeCallback = () => setShow(false);
     return (
         <div id="app">
-            <Modal show={show} close_callback={closeCallback}>
+            <Modal show={show} closeCallback={closeCallback}>
                 <div className="modal-header">Learn More about this Potato</div>
                 <p className="modal-text">Welcome my nuggets! I am thicc fat potato sitting in front of a computer. I am 14 years old, look like a giraffe and sleep like a dolphin. Yes I know the previous two sentences don't make sense just like my life. I wake up at 6:45am and go to bathroom to do what you also do, you know on the seat... I wake up at 1:00pm on holidays. <br/>
                 I have made two render engines and one physics engine, with all the nerdy stuff done myself. I moved to Australia in 2019, I annoy my teachers and do not take any school work seriously. <br/>
@@ -23,10 +25,19 @@ const App = () => {
                 If you are fascinated by this lifeless potato then join his <a href="https://discord.gg/bW5EC6u">Discord Server</a> so that he can have some friends and not just talk to his pillow. <br/>
                 You can find this potato on instagram, I like being around people btw.</p>
             </Modal>
+            <Modal show={affiliate} closeCallback={() => { setAffiliate(false); }}>
+                <div className="modal-header">Use These Links When Subscribing to their Servie...</div>
+                <div className="modal-text">
+                    <ol>
+                        <li><a href="https://www.vultr.com/?ref=8726954">Vultr: VPS</a></li>
+                    </ol>
+                </div>
+            </Modal>
+
             <div id="landing">
                 <div className="header">NavTheNugget or Lifeless Potato</div>
                 <div className="subheader">Professional Dumbass, Programmer, Potato</div>
-                <div className="subheader">Still a High Schooler, lives in Australia. <span id="learn-more" onClick={() => {setShow(true);}}>Learn More</span></div>
+                <div className="subheader">Still a High Schooler, lives in Australia. <span id="learn-more" onClick={() => {setShow(true);}}>Learn More</span> <span id="affiliates"></span></div>
                 <div id="social-media">
                     <div id="icons">
                         <a href="https://instagram.com/NavTheNugget" target="_blank" rel="noopener noreferrer" className="link"><img src={InstagramLogo} alt="" className="invert" /></a>
